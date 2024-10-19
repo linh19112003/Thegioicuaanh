7a3a82e1aaf42d6d956a882a0b53112e6d1f0084
linh19112003 <quanglinh19112003@gmail.com>
2024-10-19 15:18:19 +0700

    lan1

---
+++ README.md
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+### [Hướng dẫn tạo website tán đổ crush đơn giản và hiệu quả || Part 2](https://youtu.be/e2KYBhCx-uU)
+> Các bạn download source về và làm theo hướng dẫn trong video nhé.
+
+
+![cover picture](/img/totinh2021-part2.jpg)
+=======
+# 20_10_rudichoi
+>>>>>>> db5e82b9a09de66e5f6ab7379228a2a7d4e95184
---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Helloooo',
+    introDesc: `Anh có điều này muốn hỏi em, em nhớ phải trả lời thật lòng nhaaaa`,
+    btnIntro: 'OK',
+    title: 'Em đi chơi với anh nhé ? ',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em đồng ý nha ',
+    btnYes: 'Có, em rất muốn đi chơi với anh 💖',
+    btnNo: ' Anh mơ à  💔 ',
+    question:'Nói cho anh biết đi vì sao em muốn đi chơi với anh',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó ahiiiii 😊😊',
+    mess: 'Anh biết mà, anh cũng muốn đi chơi với em lắm',
+    messDesc: ' Nhắn tin cho anh để biết lịch trình chi tiết nha',
+    btnAccept: 'Ok em nhắn ngayyy',
+    messLink: 'https://www.facebook.com/profile.php?id=100053304012961'
+}
